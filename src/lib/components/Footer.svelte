648f5a6d0c3b23a7d1ed3ebcc4d107a456136ec1
User <user@Users-MacBook-Pro.local>
2025-02-05 10:59:34 +1300

    update

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,14 @@
+<!--**********************************
+    Footer start
+***********************************-->
+<div class="footer">
+	<div class="copyright">
+		<p>
+			Copyright ©
+			<a href="http://echotech.co.nz/" target="_blank">Echo</a> 2025
+		</p>
+	</div>
+</div>
+<!--**********************************
+    Footer end
+***********************************-->
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,25 @@
 <script>
-    import pb from '$lib/pocketbase';
-  
-    function handleLogout() {
-      pb.authStore.clear();
-      window.location.href = '/login';
-    }
-  </script>
-  
-  <nav>
-    {#if pb.authStore.isValid}
-      <button on:click={handleLogout}>Logout</button>
-    {:else}
-      <a href="/login">Login</a>
-      <a href="/signup">Sign Up</a>
-    {/if}
-  </nav>
-  <slot />
+  import Nav from '$lib/components/Nav.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+ </script>
+
+<svelte:head>
+ <title>Echo Tech</title>
+ <link href="./vendor/jqvmap/css/jqvmap.min.css" rel="stylesheet" />
+ <link rel="stylesheet" href="./vendor/chartist/css/chartist.min.css" />
+ <!-- Vectormap -->
+ <link href="./vendor/jqvmap/css/jqvmap.min.css" rel="stylesheet" />
+ <link
+   href="./vendor/bootstrap-select/dist/css/bootstrap-select.min.css"
+   rel="stylesheet"
+ />
+ <link href="./css/style.css" rel="stylesheet" />
+ <link href="./vendor/owl-carousel/owl.carousel.css" rel="stylesheet" />
+
+
+
+</svelte:head>
+
+
+<Nav />
+<Footer />
